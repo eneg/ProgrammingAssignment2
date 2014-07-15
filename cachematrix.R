3f11d950bd23609185e82f9ab7bf4f5a7cdbc421
G. Gracian <ggracian@free.fr>
2014-07-15 16:16:28 +0200

    2nd commit with comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow not to repeat matrix inversion - which can last long -
+## caching the resulting matrix of the first time the origin matrix was inverted.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix takes as argument any matrix and returns a list. 
+
+## The function cacheSolve takes the result of makeCacheMatrix as argument and 
+## returns the inverted matrix. Note that no control is done wether the matrix 
+## is singular or not and, depending on the original matrix, an error can be 
+## returned.
+
+## 1st step, creating the cacheable matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-	## Make a matrix with cache
+	
+	## initialize the inverted matrix
 	m <- NULL
+	
+	## define the set function attached to the cacheable matrix
 	set <- function(y) {
 		x <<- y
 		m <<- NULL
 	}
+	
+	## define the get function attached to the cacheable matrix
 	get <- function() x
+	
+	## define the function to be used if the matrix wasn't yet inverted
 	setinv <- function(solve) m <<- solve
+	
+	## define the function to be used if the matrix was already inverted
 	getinv <- function() m
+	
+	## returns the cacheable matrix as a list
 	list(set = set, get = get,
 	     setinv = setinv,
 	     getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## 2nd step inverting the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## tries to get the preexisting inverted matrix
 	m <- x$getinv()
+	
+	## if it exists, then signals that a cache exists, returns the cached 
+	## inverted matrix and exits
 	if(!is.null(m)) {
-		message("getting cached data")
+		message("Matrix already inverted, getting cached data")
 		return(m)
 	}
+	
+	## the inverted matrix doesn't exist, take the origin matrix
 	data <- x$get()
+	
+	## inverts it
 	m <- solve(data)
+	
+	## stores the inverted matrix
 	x$setinv(m)
+	
+	## and returns the result of the inversion
 	m
 }
